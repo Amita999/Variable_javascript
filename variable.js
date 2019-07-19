61bfd6b5fb35b18f3b501433e3d9085f852bca90
amita.jethani <amitajethani@gmail.com>
2019-07-20 00:28:34 +0530

    Assignment & Concat Done

---
+++ variable.js
@@ -0,0 +1,19 @@
+// assignment operator
+var a = 7;
+var b;
+console.log(b);//o/p: undefined
+b = a;
+console.log(b);//o/p:7
+
+// various variables 
+
+var c = 10;
+var d = 12;
+var e = "It is a" ;
+console.log(c);
+console.log(d);
+console.log(e);
+
+// concat
+ console.log(c+d);
+ console.log(e + "string");
